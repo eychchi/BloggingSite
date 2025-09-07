88e8f214b417a65611c72e1fee730a93b9cd5390
eychchi <evinveb06@gmail.com>
2025-09-08 07:02:04 +0800

    Lab Exercise 3: C# .NET 6 Class Library — Continuation of Lab Exercise 2 Updadte

--- BlogDataLibrary/obj/Debug/net6.0/BlogDataLibrary.AssemblyInfo.cs
+++ BlogDataLibrary/obj/Debug/net6.0/BlogDataLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogDataLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+169f6c2907a2d147fd6c156684ebd68dc861ecec")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogDataLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogDataLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- BlogTestUI/Program.cs
+++ BlogTestUI/Program.cs
@@ -1,10 +1,161 @@
-﻿namespace BlogTestUI
+﻿using BlogDataLibrary.Data;
+using BlogDataLibrary.Database;
+using BlogDataLibrary.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace BlogTestUI
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            SqlData db = GetConnection();
+
+            Console.WriteLine("1 - Register");
+            Console.WriteLine("2 - Authenticate (Login)");
+            Console.WriteLine("3 - Login & Add Post");
+            Console.WriteLine("4 - List Posts");
+            Console.WriteLine("5 - Show Post Details");
+            Console.WriteLine("0 - Exit");
+            Console.Write("Choose option: ");
+            string choice = Console.ReadLine();
+            Console.WriteLine();
+
+            switch (choice)
+            {
+                case "1":
+                    Register(db);
+                    break;
+
+                case "2":
+                    Authenticate(db);
+                    break;
+
+                case "3":
+                     AddPost(db);
+                    break;
+
+                case "4":
+                    ListPosts(db);
+                    break;
+
+                case "5":
+                    ShowPostDetails(db);
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid option.");
+                    break;
+            }
+
+            Console.WriteLine("Press Enter to exit...");
+            Console.ReadLine();
+        }
+
+        private static UserModel GetCurrentUser(SqlData db)
+        {
+            Console.Write("Username: ");
+            string username = Console.ReadLine();
+
+            Console.Write("Password: ");
+            string password = Console.ReadLine();
+
+            UserModel user = db.Authenticate(username, password);
+
+            return user;
+        }
+
+        public static void Register(SqlData db)
+        {
+            Console.Write("Enter new username: ");
+            var username = Console.ReadLine();
+
+            Console.Write("Enter new password: ");
+            var password = Console.ReadLine();
+
+            Console.Write("Enter first name: ");
+            var firstName = Console.ReadLine();
+
+            Console.Write("Enter last name: ");
+            var lastName = Console.ReadLine();
+
+            db.Register(username, firstName, lastName, password);
+        }
+
+        public static void AddPost(SqlData db)
+        {
+            UserModel user = GetCurrentUser(db);
+
+            Console.Write("Title: ");
+            string title = Console.ReadLine();
+
+            Console.Write("Write body: ");
+            string body = Console.ReadLine();
+
+            PostModel post = new PostModel
+            {
+                Title = title,
+                Body = body,
+                DateCreated = DateTime.Now,
+                UserId = user.Id
+            };
+
+            db.AddPost(post);
+        }
+
+        public static void Authenticate(SqlData db)
+        {
+            UserModel user = GetCurrentUser(db);
+
+            if (user == null)
+            {
+                Console.WriteLine("Invalid credentials.");
+            }
+            else
+            {
+                Console.WriteLine($"Welcome, {user.UserName}");
+            }
+        }
+
+        private static void ListPosts(SqlData db)
+        {
+            List<ListPostModel> posts = db.ListPosts();
+
+            foreach (ListPostModel post in posts)
+            {
+                Console.WriteLine($"{post.Id}. Title: {post.Title} by {post.UserName} [{post.DateCreated.ToString("yyyy-MM-dd")}]");
+                Console.WriteLine($"{post.Body.Substring(0, 20)}...");
+                Console.WriteLine();
+            }
+        }
+
+        private static void ShowPostDetails(SqlData db)
+        {
+            Console.Write("Enter a Post ID: ");
+            int id = Int32.Parse(Console.ReadLine());
+
+            ListPostModel post = db.ShowPostDetails(id);
+            Console.WriteLine(post.Title);
+            Console.WriteLine($"by {post.FirstName} {post.LastName} [{post.UserName}]");
+
+            Console.WriteLine();
+
+            Console.WriteLine(post.Body);
+
+            Console.WriteLine(post.DateCreated.ToString("MMM d, yyyy"));
+        }
+
+        static SqlData GetConnection()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            IConfiguration config = builder.Build();
+            ISqlDataAccess dbAccess = new SqlDataAccess(config);
+            SqlData db = new SqlData(dbAccess);
+
+            return db;
         }
     }
 }
